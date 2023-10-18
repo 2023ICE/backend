febb9aca8ef293f657d733f90d52d12b839dd062
김승범 <kimsb7218@naver.com>
2023-10-18 22:44:32 +0900

    [feat] CorsFilter 추가

---
+++ src/main/java/allercheck/backend/global/web/filter/CorsFilter.java
@@ -0,0 +1,22 @@
+package allercheck.backend.global.web.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain chain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(request, response);
+    }
+}

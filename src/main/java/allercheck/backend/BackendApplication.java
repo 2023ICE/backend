0e1e645e0038e9190dc9b83be5e22c80d728fb3d
김승범 <kimsb7218@naver.com>
2023-10-18 22:27:40 +0900

    [feat] SpringBoot가 자동으로 설정하는 보안 설정을 비활성화하기 위한 구문 추가

--- src/main/java/allercheck/backend/BackendApplication.java
+++ src/main/java/allercheck/backend/BackendApplication.java
@@ -2,9 +2,10 @@ package allercheck.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @PropertySource("classpath:secure.properties")
 public class BackendApplication {
 

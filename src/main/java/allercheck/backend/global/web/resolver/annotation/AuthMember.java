4deb9fe997de95fdc35861985f58924ba0959fab
김승범 <kimsb7218@naver.com>
2023-10-28 00:46:47 +0900

    [refactor] 의미없는 주석 제거

--- src/main/java/allercheck/backend/global/web/resolver/annotation/AuthMember.java
+++ src/main/java/allercheck/backend/global/web/resolver/annotation/AuthMember.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.PARAMETER) // annotation이 생설될 수 있는 위치 지정(Parameter 선언시)
-@Retention(RetentionPolicy.RUNTIME) // annotation이 언제까지 유효하는지 지정(Complie 이후에도 존재)
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AuthMember {
 }

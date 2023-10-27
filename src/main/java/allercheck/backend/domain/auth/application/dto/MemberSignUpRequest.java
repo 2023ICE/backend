2d5ac1c57df93dcef33fbbf2451b8a703ee1bb8f
김승범 <kimsb7218@naver.com>
2023-10-28 00:44:42 +0900

    [refactor] 어노테이션 우선순위에 맞춰 순서 재지정

--- src/main/java/allercheck/backend/domain/auth/application/dto/MemberSignUpRequest.java
+++ src/main/java/allercheck/backend/domain/auth/application/dto/MemberSignUpRequest.java
@@ -8,9 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
 public class MemberSignUpRequest {
 
     @NotBlank

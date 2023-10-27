a190c7e17ca6bf1cf98ef156c4f24363861cb06f
김승범 <kimsb7218@naver.com>
2023-10-28 00:34:08 +0900

    [refactor] 어노테이션 우선순위에 맞춰 순서 재지정 (@Service 어노테이션이 앞으로 오도록 지정)

--- src/main/java/allercheck/backend/domain/auth/application/AuthService.java
+++ src/main/java/allercheck/backend/domain/auth/application/AuthService.java
@@ -17,8 +17,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+
 @RequiredArgsConstructor
+@Service
 public class AuthService {
 
     private final TokenProvider tokenProvider;

d23a63f6a41895d314100e97e916939d07c69caf
김승범 <kimsb7218@naver.com>
2023-10-28 00:34:45 +0900

    [refactor] 어노테이션 우선순위에 맞춰 순서 재지정 (@RestController가 밑으로 오도록 지정)

--- src/main/java/allercheck/backend/domain/auth/presentation/AuthController.java
+++ src/main/java/allercheck/backend/domain/auth/presentation/AuthController.java
@@ -10,9 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;

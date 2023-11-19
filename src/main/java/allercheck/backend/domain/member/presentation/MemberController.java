a8338c935765819708cab837a80022266bf96d66
김승범 <kimsb7218@naver.com>
2023-11-19 23:48:50 +0900

    [refactor] 비밀번호 변경 URI 경로 변경

--- src/main/java/allercheck/backend/domain/member/presentation/MemberController.java
+++ src/main/java/allercheck/backend/domain/member/presentation/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping("/api/members")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 @RestController
 public class MemberController {

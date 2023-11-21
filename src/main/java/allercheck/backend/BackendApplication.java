a75471c6d76a530cce1101b1b0a4d490521f8a65
김승범 <kimsb7218@naver.com>
2023-11-21 18:44:30 +0900

    [feat] add test api for deployment

--- src/main/java/allercheck/backend/BackendApplication.java
+++ src/main/java/allercheck/backend/BackendApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @PropertySource("classpath:secure.properties")
 public class BackendApplication {
@@ -13,4 +16,8 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping("/test")
+	public String test() {
+		return "test complete!";
+	}
 }

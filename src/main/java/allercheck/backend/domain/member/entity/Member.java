d4d157edca920c54c53553e552dccecb41dbb44b
김승범 <kimsb7218@naver.com>
2023-11-24 21:13:54 +0900

    [refactor] Change to strategy = GenerationType.IDENTITY -> stategy = GenerationType.AUTO to ensure the order of the id values

--- src/main/java/allercheck/backend/domain/member/entity/Member.java
+++ src/main/java/allercheck/backend/domain/member/entity/Member.java
@@ -26,7 +26,7 @@ import java.util.EnumSet;
 public class Member extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
